595486a03a9de60550e8d4a7fab1ba381314ef76
RJ Hill <infamous.rj@gmail.com>
2012-01-28 13:23:53 -0500

    Initial Commit

---
+++ Happiness/HappinessViewController.h
@@ -0,0 +1,13 @@
+//
+//  HappinessViewController.h
+//  Happiness
+//
+//  Created by theinfamousrj on 1/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HappinessViewController : UIViewController
+
+@end
