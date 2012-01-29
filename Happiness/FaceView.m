e3ea604bb1735c43045cd5e703af69fde64c2d40
RJ Hill <infamous.rj@gmail.com>
2012-01-29 11:45:28 -0500

    Completed face drawing and pinch gesture implementation.

--- Happiness/FaceView.m
+++ Happiness/FaceView.m
@@ -10,18 +10,67 @@
 
 @implementation FaceView
 
+@synthesize scale = _scale;
+
+//HEAD
+#define DEFAULT_SCALE 0.90
+//EYES
+#define EYE_H 0.35
+#define EYE_V 0.35
+#define EYE_RADIUS 0.10
+//MOUTH
+#define MOUTH_H 0.45
+#define MOUTH_V 0.40
+#define MOUTH_SMILE 0.25
+
+- (CGFloat)scale
+{
+    if (!_scale) {
+        return DEFAULT_SCALE;
+    } else {
+        return _scale;
+    }
+}
+
+- (void)setScale:(CGFloat)scale
+{
+    if (scale != _scale) {
+        _scale = scale;
+        [self setNeedsDisplay];
+    }
+}
+
+- (void)pinch:(UIPinchGestureRecognizer *)gesture
+{
+    if ((gesture.state == UIGestureRecognizerStateChanged) || (gesture.state == UIGestureRecognizerStateEnded)) {
+        self.scale *= gesture.scale;
+        gesture.scale = 1;
+    }
+}
+
+- (void)setup
+{
+    self.contentMode = UIViewContentModeRedraw;
+}
+
+- (void) awakeFromNib
+{
+    [self setup];
+}
+
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
     if (self) {
-        // Initialization code
+        [self setup];
     }
     return self;
 }
 
 - (void)drawCircleAtPoint:(CGPoint)p
                withRadius:(CGFloat)radius
-                inContext:(CGContextRef)context {
+                inContext:(CGContextRef)context
+{
     UIGraphicsPushContext(context);
     CGContextBeginPath(context);
     CGContextAddArc(context, p.x, p.y, radius, 0, 2*M_PI, YES);
@@ -29,15 +78,60 @@
     UIGraphicsPopContext();
 }
 
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
+
 - (void)drawRect:(CGRect)rect
 {
     CGContextRef context = UIGraphicsGetCurrentContext();
-    //draw face (circle)
+    
+    //draw face
+    CGPoint midpoint;
+    midpoint.x = self.bounds.origin.x + self.bounds.size.width/2;
+    midpoint.y = self.bounds.origin.y + self.bounds.size.height/2;
+    
+    CGFloat size = self.bounds.size.width/2;
+    if (self.bounds.size.height < self.bounds.size.width) size = self.bounds.size.height/2;
+    size *= self.scale;
+    
+    CGContextSetLineWidth(context, 5.0);
+    [[UIColor blueColor] setStroke];
+    
+    [self drawCircleAtPoint:midpoint withRadius:size inContext:context];
+    
+    
     //draw eyes (2 circles)
-    //no nose
+    CGPoint eyepoint;
+    eyepoint.x = midpoint.x - size * EYE_H;
+    eyepoint.y = midpoint.y - size * EYE_V;
+    
+    [self drawCircleAtPoint:eyepoint withRadius:size * EYE_RADIUS inContext:context];
+    eyepoint.x += size * EYE_H * 2;
+    [self drawCircleAtPoint:eyepoint withRadius:size * EYE_RADIUS inContext:context];
+
+    
     //draw mouth
+    CGPoint mouthStart;
+    mouthStart.x = midpoint.x - MOUTH_H * size;
+    mouthStart.y = midpoint.y + MOUTH_V * size;
+    
+    CGPoint mouthEnd = mouthStart;
+    mouthEnd.x += MOUTH_H * size * 2;
+    
+    //control points for mouth
+    CGPoint mouthCP1 = mouthStart;
+    mouthCP1.x += MOUTH_H * size * 2/3;
+    CGPoint mouthCP2 = mouthEnd;
+    mouthCP2.x -= MOUTH_H * size * 2/3;
+    
+    float smile = 2.0;
+    
+    CGFloat smileOffset = MOUTH_SMILE * size * smile;
+    mouthCP1.y += smileOffset;
+    mouthCP2.y += smileOffset;
+    
+    CGContextBeginPath(context);
+    CGContextMoveToPoint(context, mouthStart.x, mouthStart.y);
+    CGContextAddCurveToPoint(context, mouthCP1.x, mouthCP2.y, mouthCP2.x, mouthCP2.y, mouthEnd.x, mouthEnd.y);
+    CGContextStrokePath(context);
 }
 
 
